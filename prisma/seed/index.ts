57fc18517a4acab2344ad66dc69c8ce9a526ea79
lucas <lucas.lebon75@gmail.com>
2025-04-09 19:01:13 +0200

    delete unnecessary seeder (user)

--- prisma/seed/index.ts
+++ prisma/seed/index.ts
@@ -1,7 +1,6 @@
 // prisma/seed/index.ts
 import { PrismaClient } from '@prisma/client';
 import { createActivities } from './activities';
-import { createUsers } from './users';
 import { createMatches } from './matches';
 import { createTeams } from './teams';
 
@@ -11,7 +10,6 @@ async function main() {
 
   const activities = await createActivities(prisma);
   const teams = await createTeams(prisma, 25);
-  const users = await createUsers(prisma, teams, 5);
   await createMatches(prisma, teams, activities, 6);
 
 }
